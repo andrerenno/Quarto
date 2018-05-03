d9e730b29622a675a576afe755709efb6c486112
andrerenno <38927989+andrerenno@users.noreply.github.com>
2018-05-03 20:31:08 -0300

    Update ai.c

--- src/ai.c
+++ src/ai.c
@@ -9,21 +9,24 @@ int ai_num(int table[]){
 	int num;
 	int usedflag = 0;
 	int badflag = 0;
-	sleep(1);
-	for (num = 0; num < 16; num++){
-		usedflag = 0;
-		badflag = 0;
+	
+	sleep(1);	//Makes it seem like the ai is doing something
+	
+	for (num = 0; num < 16; num++){		//Goes through all numbers to find one that
+		usedflag = 0;			//hasn't already been used
+		badflag = 0;			//and won't let the opponent win.
+		
 		for (loc = 0; loc < 16; loc++){
 			if (table[loc] == num)
 				usedflag = 1;
 		}
 		if (usedflag)
 			continue;
-		for (loc = 0; loc < 16; loc++){
+		for (loc = 0; loc < 16; loc++){	//Tests number on table to see if the player can win by placing it
 			if (table[loc] != 16)
 				continue;
 			table[loc] = num;
-			if (check_victory(table)){
+			if (check_victory(table)){	//Means that outputing this number would let the player win.
 				badflag = 1;
 				table[loc] = 16;
 				break;
@@ -33,21 +36,23 @@ int ai_num(int table[]){
 		if (badflag == 0) 
 			return num;
 	}	
-	do {
+	for (num = 0; num < 16; num++) {  //Reaching this means that the player can win with any number I give him.
 		usedflag = 0;
-		num = rand() % 16;
 		for (loc = 0; loc < 16; loc++){
 			if (table[loc] == num)
 				usedflag = 1;
 		}
-	} while (usedflag);
-	return num;
+		if (usedflag == 0)
+			return num;
+	}
+
 }
 
 int ai_place(int table[], int num){
 	int loc;
-	sleep(1);
-	for (loc = 0; loc < 16; loc++){
+	sleep(1);	//Makes it seem like the ai is doing something
+	
+	for (loc = 0; loc < 16; loc++){		//Goes through all the numbers to try to find a winning move.
 
 		if (table[loc] != 16)
 			continue;
